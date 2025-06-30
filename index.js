1aede0e6e2a6f918a289111a9ab2224b036331b1
Chris Mathew Aje <chrismaje63@gmail.com>
2025-06-30 13:20:17 +0530

    index.js setup to export categories.

---
+++ index.js
@@ -0,0 +1,31 @@
+import christian from './data/christian.js';
+import deep from './data/deep.js';
+import fun from './data/fun.js';
+import inspiration from './data/inspiration.js';
+import movies from './data/movies.js';
+import verses from './data/verses.js';
+
+const allCategories = {
+  christian,
+  deep,
+  fun,
+  inspiration,
+  movies,
+  verses
+};
+
+export function getRandomQuote(category = null) {
+  const keys = Object.keys(allCategories);
+  const chosenCategory = category && keys.includes(category.toLowerCase())
+    ? category.toLowerCase()
+    : keys[Math.floor(Math.random() * keys.length)];
+  const quotes = allCategories[chosenCategory];
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  return { ...quote, category: chosenCategory };
+}
+
+export function getCategories() {
+  return Object.keys(allCategories);
+}
+
+export { allCategories };
